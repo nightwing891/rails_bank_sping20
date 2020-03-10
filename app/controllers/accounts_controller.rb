cb505775e553406522762ca6c5a1d1648342d840
Henry Doan <henrydoan96@gmail.com>
2020-03-09 20:54:56 -0600

    end of lecture

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,51 @@
+class AccountsController < ApplicationController
+  before_action :set_account, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @accounts = current_user.accounts
+  end
+
+  def show
+  end
+
+  def new
+    @account = current_user.accounts.new
+  end
+
+  def create
+    @account = current_user.accounts.new(account_params)
+    if @account.save
+      flash[:success] = "Account Created!"
+      redirect_to @account
+    else
+      flash[:error] = "Error: #{@account.errors.full_messages.join("\n")}"
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @account.update(account_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @account.destroy
+    redirect_to root_path
+  end
+
+  private
+    def account_params
+      params.require(:account).permit(:name, :balance)
+    end
+
+    # preventing seeing other users accounts 
+    def set_account
+      @account = current_user.accounts.find(params[:id])
+    end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  # def user_params
+  #   params.require(:user).permit(:email, :password)
+  # end
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    end
+
+    # def user_params
+    #   params.require(:user).permit(:email, :password, :first_name, :last_name)
+    # end
 end
